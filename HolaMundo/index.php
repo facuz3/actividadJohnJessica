5501c42cf719642e688b91a49041843075ad7d6a
jaimepg2 <jaimepg2@INF2-PC11>
2017-03-10 20:57:30 +0100

    esta es la rama alternativa

--- HolaMundo/index.php
+++ HolaMundo/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        Echo'Jaime Pedrajas->John Facundo Zabaleta->Jessica';
+        Echo'Jaime Pedrajas(Gandia) Facundo Zabaleta(Gandia)';
         ?>
     </body>
 </html>
